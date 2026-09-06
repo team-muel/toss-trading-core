beea069138ec89f0b8fb06b2a600f42ed66a798a
amandalmoon <seojeongseo7@gmail.com>
2026-09-06 22:08:52 +0900

    Add strategy registry lifecycle

--- src/asset_management/governance/__init__.py
+++ src/asset_management/governance/__init__.py
@@ -4,6 +4,12 @@ from .model_registry import (
     ModelAuthorization, ModelDefinition, ModelRegistry, ModelScope, ModelStatus,
     ModelTransition,
 )
+from .strategy_registry import (
+    CapitalRiskBudget, StrategyAttribution, StrategyAuthorization, StrategyDefinition,
+    StrategyRegistry, StrategyRuntimeMode, StrategyStatus, StrategyTransition,
+)
 
 __all__ = ["ModelAuthorization", "ModelDefinition", "ModelRegistry", "ModelScope",
-           "ModelStatus", "ModelTransition"]
+           "ModelStatus", "ModelTransition", "CapitalRiskBudget", "StrategyAttribution",
+           "StrategyAuthorization", "StrategyDefinition", "StrategyRegistry",
+           "StrategyRuntimeMode", "StrategyStatus", "StrategyTransition"]
