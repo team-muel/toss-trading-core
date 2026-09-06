3d8e6a218ebdf334e3f0807d10a572ea593bea36
amandalmoon <seojeongseo7@gmail.com>
2026-09-06 22:49:39 +0900

    Add investor mandate authority

--- src/asset_management/governance/__init__.py
+++ src/asset_management/governance/__init__.py
@@ -8,8 +8,14 @@ from .strategy_registry import (
     CapitalRiskBudget, StrategyAttribution, StrategyAuthorization, StrategyDefinition,
     StrategyRegistry, StrategyRuntimeMode, StrategyStatus, StrategyTransition,
 )
+from .investor_mandate import (
+    BenchmarkDefinition, InvestorMandate, InvestorMandateRegistry, MandateObjective,
+    OptimizerMandateAuthorization, RiskPreference, WealthConvention,
+)
 
 __all__ = ["ModelAuthorization", "ModelDefinition", "ModelRegistry", "ModelScope",
            "ModelStatus", "ModelTransition", "CapitalRiskBudget", "StrategyAttribution",
            "StrategyAuthorization", "StrategyDefinition", "StrategyRegistry",
-           "StrategyRuntimeMode", "StrategyStatus", "StrategyTransition"]
+           "StrategyRuntimeMode", "StrategyStatus", "StrategyTransition", "BenchmarkDefinition",
+           "InvestorMandate", "InvestorMandateRegistry", "MandateObjective",
+           "OptimizerMandateAuthorization", "RiskPreference", "WealthConvention"]
