cffa20259c2ef751346041af951b10afd9333cba
Beomsoo Kim <132043689+Muelfly@users.noreply.github.com>
2026-09-08 16:06:53 +0900

    AMA-133 export research bridge from orchestration boundary

--- src/asset_management/orchestration/__init__.py
+++ src/asset_management/orchestration/__init__.py
@@ -4,7 +4,15 @@ from .decision_kernel import (
     DecisionKernel, DecisionKernelEvaluation, DecisionParityLedger, DecisionRuntime,
     DecisionRuntimeAdapter, FrozenDecisionInput, PreExecutionDecision, RuntimeAdapterDescriptor,
 )
+from .research_bridge import (
+    CostTiming, DecayStage, GrossNetBasis, ResearchSignalBridgeContract,
+    ResearchSignalBridgeRecord, bridge_history_result, require_decay_stage_available,
+)
 
-__all__ = ["DecisionKernel", "DecisionKernelEvaluation", "DecisionParityLedger",
-           "DecisionRuntime", "DecisionRuntimeAdapter", "FrozenDecisionInput",
-           "PreExecutionDecision", "RuntimeAdapterDescriptor"]
+__all__ = [
+    "DecisionKernel", "DecisionKernelEvaluation", "DecisionParityLedger",
+    "DecisionRuntime", "DecisionRuntimeAdapter", "FrozenDecisionInput",
+    "PreExecutionDecision", "RuntimeAdapterDescriptor",
+    "CostTiming", "DecayStage", "GrossNetBasis", "ResearchSignalBridgeContract",
+    "ResearchSignalBridgeRecord", "bridge_history_result", "require_decay_stage_available",
+]
