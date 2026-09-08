3a4c1f71b4947d5a94e856a971882d9cfc8b3167
amandalmoon <seojeongseo7@gmail.com>
2026-09-08 17:23:44 +0900

    feat: add PIT factor and specific risk assessment

--- src/asset_management/risk/__init__.py
+++ src/asset_management/risk/__init__.py
@@ -3,7 +3,7 @@ from .contributions import portfolio_risk
 from .covariance import sample_covariance, ewma_covariance, shrink_covariance, factor_covariance, stress_covariance, safe_inverse
 from .cvar import TailRiskAssessment, assess_historical_tail_risk, historical_tail_risk
 from .event_risk import EventAction, EventRiskControl, EventRiskInput, EventType, assess_event_risk, event_risk_action
-from .factor_risk import aggregate_exposure
+from .factor_risk import FactorExposure, FactorRiskAssessment, SpecificRiskPolicy, aggregate_exposure, assess_factor_specific_risk
 from .liquidity import LiquidityAssessment, LiquidityDecisionStatus, LiquidityRiskInput, assess_liquidity_risk, liquidity_risk
 from .models import CurrencyBasis, MissingPolicy, ReturnPanel, RiskGate, optimizer_risk_gate
 from .returns import build_return_panel, decompose_base_currency_return, require_common_risk_context
