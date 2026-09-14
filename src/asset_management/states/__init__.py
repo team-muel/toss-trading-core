25444090028d8911d5a6c5f71fa2209c9f36f7df
Beomsoo Kim <132043689+Muelfly@users.noreply.github.com>
2026-09-14 18:16:49 +0900

    AMA-175: export state normalization contract

--- src/asset_management/states/__init__.py
+++ src/asset_management/states/__init__.py
@@ -3,10 +3,12 @@
 from .company import CompanyStateEngine
 from .engine import StateEngine, StateRepository
 from .market import MarketStateEngine
-from .models import OperationalState, StateComponent, StatePolicy, StateSnapshot, StateType
+from .models import (OperationalState, StateComponent, StateNormalization, StatePolicy,
+                     StateSnapshot, StateType)
 from .portfolio import PortfolioStateEngine
 from .system import SystemStateEngine
 
 __all__ = ["CompanyStateEngine", "MarketStateEngine", "OperationalState",
-           "PortfolioStateEngine", "StateComponent", "StateEngine", "StatePolicy",
-           "StateRepository", "StateSnapshot", "StateType", "SystemStateEngine"]
+           "PortfolioStateEngine", "StateComponent", "StateEngine", "StateNormalization",
+           "StatePolicy", "StateRepository", "StateSnapshot", "StateType",
+           "SystemStateEngine"]
