be91e446ba1b6d8ebe3eb7314299ea5d86ebacd2
Beomsoo Kim <132043689+Muelfly@users.noreply.github.com>
2026-09-20 15:33:18 +0900

    AMA-177: export regime inference contracts

--- src/asset_management/states/__init__.py
+++ src/asset_management/states/__init__.py
@@ -9,10 +9,16 @@ from .market_builder import (MarketStateBuilder, MarketStateBuildResult,
 from .models import (OperationalState, StateComponent, StateFeatureInput, StateNormalization,
                      StatePolicy, StateSnapshot, StateType)
 from .portfolio import PortfolioStateEngine
+from .regime import (
+    RegimeModelSpec, RegimeOutputSemantics, RegimeProbability, RegimeRepository,
+    RegimeSnapshot,
+)
 from .system import SystemStateEngine
 
 __all__ = ["CompanyStateEngine", "MarketStateBuilder", "MarketStateBuildResult",
            "MarketStateComponentSpec", "MarketStateEngine", "MarketStateSpec",
-           "OperationalState", "PortfolioStateEngine", "StateComponent", "StateEngine",
-           "StateFeatureInput", "StateNormalization", "StatePolicy", "StateRepository",
-           "StateSnapshot", "StateType", "SystemStateEngine", "foundation_market_state_spec"]
+           "OperationalState", "PortfolioStateEngine", "RegimeModelSpec",
+           "RegimeOutputSemantics", "RegimeProbability", "RegimeRepository", "RegimeSnapshot",
+           "StateComponent", "StateEngine", "StateFeatureInput", "StateNormalization",
+           "StatePolicy", "StateRepository", "StateSnapshot", "StateType", "SystemStateEngine",
+           "foundation_market_state_spec"]
