66b8622ef77bd6ca53f6f691798add87da76e6d9
Beomsoo Kim <132043689+Muelfly@users.noreply.github.com>
2026-09-20 15:35:07 +0900

    AMA-178: export canonical macro state contract

--- src/asset_management/features/__init__.py
+++ src/asset_management/features/__init__.py
@@ -1,10 +1,11 @@
 """Versioned features computed only from eligible point-in-time inputs."""
 """Versioned point-in-time feature store."""
 
+from .macro_state import MacroStateResult, MacroStateSpec, run_macro_state
 from .models import FeatureContext, FeatureDefinition, FeatureInput, FeatureSnapshot, FeatureValue
 from .registry import FeatureRegistry, builtin_definitions
 from .store import FeatureRunResult, FeatureStore
 
 __all__ = ["FeatureContext", "FeatureDefinition", "FeatureInput", "FeatureRegistry",
            "FeatureRunResult", "FeatureSnapshot", "FeatureStore", "FeatureValue",
-           "builtin_definitions"]
+           "MacroStateResult", "MacroStateSpec", "builtin_definitions", "run_macro_state"]
